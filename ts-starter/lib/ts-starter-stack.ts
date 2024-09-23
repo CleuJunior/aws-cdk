e678c240cf3d69d5b0c52e58c959f93eb9767e7a
CleuJunior <cleu.junior@gmail.com>
2024-09-23 19:05:50 -0300

    CDK Constructs

--- ts-starter/lib/ts-starter-stack.ts
+++ ts-starter/lib/ts-starter-stack.ts
@@ -1,16 +1,18 @@
 import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import {aws_s3} from 'aws-cdk-lib';
+import {Construct} from 'constructs';
 
 export class TsStarterStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-    // The code that defines your stack goes here
+        new aws_s3.Bucket(this, "TsBucket", {
+            lifecycleRules: [
+                {
+                    expiration: cdk.Duration.days(45)
+                }
+            ]
+        })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'TsStarterQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+    }
 }
