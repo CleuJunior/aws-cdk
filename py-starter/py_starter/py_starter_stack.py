2cb2a2ff7b4c672b4d2f4ace2e77056828139337
CleuJunior <cleu.junior@gmail.com>
2024-09-23 19:07:15 -0300

    CDK Constructs

--- py-starter/py_starter/py_starter_stack.py
+++ py-starter/py_starter/py_starter_stack.py
@@ -1,19 +1,17 @@
 from aws_cdk import (
-    # Duration,
+    Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_s3 as s3
 )
 from constructs import Construct
+
 
 class PyStarterStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
-
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "PyStarterQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        s3.Bucket(self, "PyBucket", lifecycle_rules=[
+            s3.LifecycleRule(
+                expiration=Duration.days(30)
+            )
+        ])
